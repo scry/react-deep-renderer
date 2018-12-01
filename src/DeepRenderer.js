eae62a04b47ae608d2df4e5a2c106ab65aa68f3f
Ross-Esmond <r.esmond@protonmail.com>
2018-12-01 12:39:51 -0600

    Added tree visiting to the DeepRenderer which can traverse element children.

--- src/DeepRenderer.js
+++ src/DeepRenderer.js
@@ -1,6 +1,20 @@
+class Collection {
+  static of (a) {
+    const asArray = Array.isArray(a) ? a : [a]
+    
+    return asArray.filter(x => x != null)
+  }
+}
+
 class DeepRenderer {
   static render (descriptors) {
-    return null
+    if (descriptors == null) return null
+
+    return {
+      type: descriptors.type,
+      children: Collection.of(DeepRenderer.render(descriptors.props.children)),
+      begits: []
+    }
   }
 }
 
--- src/DeepRenderer.test.js
+++ src/DeepRenderer.test.js
@@ -1,5 +1,44 @@
+const React = require('react')
 const DeepRenderer = require('./DeepRenderer')
+
+const e = React.createElement
 
 test('should return null if given null', function () {
   expect(DeepRenderer.render(null)).toBe(null)
+})
+
+test('should return a single entity if given a dom node', function () {
+  const div = e('div')
+
+  expect(DeepRenderer.render(div)).toEqual({
+    type: 'div',
+    children: [],
+    begits: []
+  })
+})
+
+test('should return the correct dom type', function () {
+  const types = ['div', 'span', 'p']
+  
+  types.forEach(type => {
+    expect(DeepRenderer.render(e(type))).toEqual({
+      type,
+      children: [],
+      begits: []
+    })
+  })
+})
+
+test('should return a tree if given a dom node with children', function () {
+  const div = e('div', null, e('span'))
+
+  expect(DeepRenderer.render(div)).toEqual({
+    type: 'div',
+    children: [{
+      type: 'span',
+      children: [],
+      begits: []
+    }],
+    begits: []
+  })
 })
