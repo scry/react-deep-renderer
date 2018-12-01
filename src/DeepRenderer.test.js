36ffc5cc302da482f62565ec783f7d4261b1da3e
Ross-Esmond <r.esmond@protonmail.com>
2018-12-01 13:28:38 -0600

    Updated jest to accept jsx syntax and refactored tests to jsx.

--- src/DeepRenderer.test.js
+++ src/DeepRenderer.test.js
@@ -1,14 +1,12 @@
 const React = require('react')
 const DeepRenderer = require('./DeepRenderer')
 
-const e = React.createElement
-
 test('should return null if given null', function () {
   expect(DeepRenderer.render(null)).toBe(null)
 })
 
 test('should return a single entity if given a dom node', function () {
-  const div = e('div')
+  const div = <div />
 
   expect(DeepRenderer.render(div)).toEqual({
     type: 'div',
@@ -20,9 +18,9 @@ test('should return a single entity if given a dom node', function () {
 test('should return the correct dom type', function () {
   const types = ['div', 'span', 'p']
   
-  types.forEach(type => {
-    expect(DeepRenderer.render(e(type))).toEqual({
-      type,
+  types.forEach(Type => {
+    expect(DeepRenderer.render(<Type />)).toEqual({
+      type: Type,
       children: [],
       begits: []
     })
@@ -30,7 +28,7 @@ test('should return the correct dom type', function () {
 })
 
 test('should return a tree if given a dom node with children', function () {
-  const div = e('div', null, e('span'))
+  const div = <div><span /></div>
 
   expect(DeepRenderer.render(div)).toEqual({
     type: 'div',
